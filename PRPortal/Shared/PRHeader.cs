82e3e8bf4dc3811979007d3c72ec5bb397a4e960
Bugz151995 <ryan.abugao@gmail.com>
2022-11-16 18:34:17 +0800

    Add project files.

---
+++ PRPortal/Shared/PRHeader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRPortal.Shared
+{
+    public class PRHeader
+    {
+        public int DocEntry { get; set; }
+        public string U_ProjectID { get; set; } // revert back to int after testing
+        public int U_ProjName { get; set; }
+        public int U_Location { get; set; }
+        public int U_PRType { get; set; } // no binded field
+        public int U_DocNum { get; set; }
+        public List<string> U_Department { get; set; } // Preferred Supplier to confirm if this is the field for it
+        public List<string> U_Branch { get; set; } // no binded field // list
+        public int U_DocStatus { get; set; }
+        public DateTime? U_TaxDate { get; set; }
+        public DateTime? U_ReqDate { get; set; }
+        public int U_Urgency { get; set; }
+        public int U_PreparedBy { get; set; }
+        public int U_ReviewedBy { get; set; }
+
+        public int U_ApprovedBy1 { get; set; }
+        public int U_ApprovedBy2 { get; set; }
+        public int U_ApprovedBy3 { get; set; }
+        public int U_ApprovedBy4 { get; set; }
+        public int U_ApprovedDate { get; set; }
+
+        public int U_Remarks { get; set; }
+        public int U_ApproverRemarks { get; set; }
+    }
+}
---
+++ PRPortal/Shared/PRLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRPortal.Shared
+{
+    public class PRLines
+    {
+        public int DocEntry { get; set; }
+        public int U_ItemCode { get; set; }
+        public int U_Dscription { get; set; }
+        public int U_WhsCode { get; set; }
+        public int U_BinLoc { get; set; }
+        public int U_Scope { get; set; }
+        public int U_ScopeDesc { get; set; }
+        public int U_MaterialCode { get; set; }
+        public int U_MaterialDesc { get; set; }
+        public int U_Quantity { get; set; }
+        public int U_InfoPrice { get; set; }
+        public int U_UomCode { get; set; }
+        public int U_ItemSpecification { get; set; }
+        public int TaxCode { get; set; }
+        public int U_OnHandQuantity { get; set; }
+        public int U_InventoryType { get; set; }
+        public int U_InventoryPurpose { get; set; }
+    }
+}
