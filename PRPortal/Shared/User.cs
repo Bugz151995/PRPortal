afba6bf2352dac631a5adc94eb688ba181c36571
Bugz151995 <ryan.abugao@gmail.com>
2022-11-18 11:31:03 +0800

    model for user

---
+++ PRPortal/Shared/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRPortal.Shared
+{
+    public class User
+    {
+        public int U_ID { get; set; }
+        public string U_UserCode { get; set; }
+        public string U_EmployeeName { get; set; }
+        public string U_UserName { get; set; }
+        public string U_Password { get; set; }
+        public string U_EmailAddress { get; set; }
+        public string U_Role { get; set; }
+        public string U_ApprovalLevel { get; set; }
+        public int U_Department { get; set; }
+        public int U_CostCenter { get; set; }
+        public int U_Approver1 { get; set; }
+        public int U_Approver2 { get; set; }
+        public int U_Approver3 { get; set; }
+        public int U_Approver4 { get; set; }
+    }
+}
