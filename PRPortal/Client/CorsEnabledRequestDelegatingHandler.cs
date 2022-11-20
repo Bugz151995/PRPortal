278190eda235583c084b84c13f47d1db6d2bbe0f
Bugz151995 <ryan.abugao@gmail.com>
2022-11-20 21:39:26 +0800

    test delegatinghandler

---
+++ PRPortal/Client/CorsEnabledRequestDelegatingHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace PRPortal.Client
+{
+    public class CorsEnabledRequestDelegatingHandler : DelegatingHandler
+    {
+        public CorsEnabledRequestDelegatingHandler() : base(new HttpClientHandler())
+        {  
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Access-Control-Allow-Origin", "http:172.31.24.203");
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
