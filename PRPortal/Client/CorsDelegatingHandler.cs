2b34e8628d66dd6a025db26b9ddcb937da4b0542
Bugz151995 <ryan.abugao@gmail.com>
2022-11-21 09:49:54 +0800

    add Access-Control-Max-Age  to request header

--- PRPortal/Client/CorsDelegatingHandler.cs
+++ PRPortal/Client/CorsDelegatingHandler.cs
@@ -8,7 +8,8 @@
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            request.Headers.Add("Access-Control-Allow-Origin", "https://172.31.24.203");
+            request.Headers.Add("Access-Control-Allow-Origin", "*");
+            request.Headers.Add("Access-Control-Max-Age", "600");
             return base.SendAsync(request, cancellationToken);
         }
     }
