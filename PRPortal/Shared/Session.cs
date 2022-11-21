7d68b093c6f5148a3d28af5f3275ae2e83dd874b
Bugz151995 <ryan.abugao@gmail.com>
2022-11-22 00:58:32 +0800

    view session and context object in console test

---
+++ PRPortal/Shared/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRPortal.Shared
+{
+    public class Session
+    {
+        public string SessionId { get; set; }
+        public string Version { get; set; }
+        public int SessionTimeout { get; set; }
+    }
+}
